0fe7f22d3fa58fdc395706ffe95717910ce85e5f
Venkat.R <ramsunvtech@gmail.com>
2024-02-01 14:38:12 +0800

    example/component: add username component ts

--- src/app/examples/username-input/username-input.component.ts
+++ src/app/examples/username-input/username-input.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputSingleLineComponent } from '../input-single-line/input-single-line.component';
 
 @Component({
-  selector: 'app-username-input',
+  selector: 'app-user-name-input',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, InputSingleLineComponent],
   templateUrl: './username-input.component.html',
-  styleUrl: './username-input.component.scss'
+  styleUrls: ['./username-input.component.scss']
 })
-export class UsernameInputComponent {
+export class UserNameInputComponent {
+  value = '';
+  error = '';
+  warning = '';
 
+  handleValueChange(newValue: string) {
+    this.value = newValue;
+    this.error = this.value === '' ? 'Field cannot be empty' : '';
+    this.warning = this.value.length > 0 && this.value.length < 5 ? 'Text is too short' : '';
+  }
+
+  handleBlur() {
+    console.log('Input field lost focus');
+  }
 }
