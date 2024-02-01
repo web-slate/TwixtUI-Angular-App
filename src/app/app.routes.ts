e9c210e93ef7e708237261e1bddc6c4363faac93
Venkat.R <ramsunvtech@gmail.com>
2024-02-01 14:04:21 +0800

    routes: add hub routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HubComponent } from './hub/hub.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'hub', component: HubComponent },
+    { path: '', redirectTo: '/hub', pathMatch: 'full' } // Redirect to 'hub' as the default route
+];
