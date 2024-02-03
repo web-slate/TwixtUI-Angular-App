e800cd975480b7c42b6fcf560d7db4bb35ff5f79
Venkat.R <ramsunvtech@gmail.com>
2024-02-03 13:20:20 +0800

    add checkbox component

--- src/app/hub/hub.component.ts
+++ src/app/hub/hub.component.ts
@@ -4,11 +4,20 @@ import { Router } from '@angular/router';
 import { SampleHeaderComponent } from '../examples/sample-header/sample-header.component';
 import { UserNameInputComponent } from '../examples/username-input/username-input.component';
 import { TwixtAlertComponent } from 'TwixtUI/twixt-alert/twixt-alert.component';
+import { TwixtButtonComponent } from 'TwixtUI/twixt-button/twixt-button.component';
+import { TwixtCheckboxComponent } from 'TwixtUI/twixt-checkbox/twixt-checkbox.component';
 
 @Component({
   selector: 'app-hub',
   standalone: true,
-  imports: [CommonModule, SampleHeaderComponent, UserNameInputComponent, TwixtAlertComponent],
+  imports: [
+    CommonModule,
+    SampleHeaderComponent,
+    UserNameInputComponent,
+    TwixtAlertComponent,
+    TwixtButtonComponent,
+    TwixtCheckboxComponent
+  ],
   templateUrl: './hub.component.html',
   styleUrl: './hub.component.scss'
 })
@@ -16,9 +25,19 @@ export class HubComponent {
   title = 'Hub Page Title';
   subTitle = 'Hub Page Sub Title';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   navigateTo(path: string) {
     this.router.navigate([path]);
+  }
+
+  // Define a noop function
+  noopClickHandler(event: Event): void {
+    // This function does nothing
+  }
+
+  // Define a noop function
+  noopCheckHandler(isChecked: boolean): void {
+    // This function does nothing
   }
 }
