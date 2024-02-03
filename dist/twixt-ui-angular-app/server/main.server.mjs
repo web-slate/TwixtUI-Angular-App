ff9a011961557908de4ca672edefc281268f9d1d
Venkat.R <ramsunvtech@gmail.com>
2024-02-04 00:40:29 +0800

    build

--- dist/twixt-ui-angular-app/server/main.server.mjs
+++ dist/twixt-ui-angular-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2VCBJWN2.mjs";import"./chunk-P2TJMBHB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-6LK7WW3L.mjs";import"./chunk-P2TJMBHB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
